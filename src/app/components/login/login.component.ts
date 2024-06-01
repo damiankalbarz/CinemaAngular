c41e14468692da8bfb74cfca30ab26f9c64d3f19
Damian <kalbarzd@gmail.com>
2024-06-01 19:59:18 +0200

    added jwt to localstorage

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,16 +1,54 @@
 import { Component } from '@angular/core';
+import { HttpClientModule, HttpHeaders } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
+  standalone: true,
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  imports: [FormsModule, HttpClientModule],
 })
 export class LoginComponent {
-  email: string = '';
-  password: string = '';
 
-  onSubmit() {
-    console.log('Email:', this.email);
-    console.log('Password:', this.password);
+  loginObj: Login;
+
+  constructor(private http: HttpClient, private router: Router) {
+    this.loginObj = new Login();
+  }
+
+  onLogin() {
+    console.log(this.loginObj);
+    const headers = new HttpHeaders().set('Content-Type', 'application/json');
+
+    this.http.post('http://localhost:9898/auth/token', this.loginObj, { headers: headers, responseType: 'text' }).subscribe({
+      next: (res: any) => {
+        console.log('Odpowiedź serwera:', res);
+        // Ustaw token JWT bezpośrednio z odpowiedzi tekstowej
+        const token = res;
+        if (token) {
+          alert('Logowanie powiodło się');
+          localStorage.setItem('angular17token', token);
+          this.router.navigateByUrl('/slider');
+        } else {
+          alert('Logowanie nie powiodło się: nie otrzymano tokenu');
+        }
+      },
+      error: (error) => {
+        console.error('Błąd podczas uwierzytelniania:', error);
+        alert('Wystąpił błąd podczas logowania.');
+      }
+    });
+  }
+}
+
+export class Login {
+  username: string;
+  password: string;
+  constructor() {
+    this.username = '';
+    this.password = '';
   }
 }
